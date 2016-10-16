f7f3022467f01ae49572dca1ac25d2049ba063df
erick <erick@hotmail.ca>
2016-10-15 22:59:31 -0400

    Add third-party persistence and DatePicker code

---
+++ src/ca/mcgill/ecse321/foodtruck/persistence/PersistenceXStream.java
@@ -0,0 +1,45 @@
+package ca.mcgill.ecse321.foodtruck.persistence;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import com.thoughtworks.xstream.XStream;
+
+public class PersistenceXStream {
+	private static XStream xstream = new XStream();
+	private static String filename = "data.xml";
+
+	public static boolean saveToXMLwithXStream(Object obj) {
+		xstream.setMode(XStream.ID_REFERENCES);
+		String xml = xstream.toXML(obj); // save our xml file
+		try {
+			FileWriter writer = new FileWriter(filename);
+			writer.write(xml);
+			writer.close();
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public static Object loadFromXMLwithXStream() {
+		xstream.setMode(XStream.ID_REFERENCES);
+		try {
+			FileReader fileReader = new FileReader(filename); // load our xml
+																// file
+			return xstream.fromXML(fileReader);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static void setAlias(String xmlTagName, Class<?> className) {
+		xstream.alias(xmlTagName, className);
+	}
+
+	public static void setFilename(String fn) {
+		filename = fn;
+	}
+}
