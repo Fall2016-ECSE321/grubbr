1c6ffbd25341d42ade0d35ea51ddb8796b4003b8
erick <erick@hotmail.ca>
2016-10-16 17:31:55 -0400

    Load registration model at startup

--- src/ca/mcgill/ecse321/foodtruck/application/FoodTruckManagement.java
+++ src/ca/mcgill/ecse321/foodtruck/application/FoodTruckManagement.java
@@ -1,10 +1,14 @@
 package ca.mcgill.ecse321.foodtruck.application;
 
+import ca.mcgill.ecse321.foodtruck.persistence.PersistenceFoodTruck;
 import ca.mcgill.ecse321.foodtruck.view.FoodTruckManagementPage;
 
 public class FoodTruckManagement {
 	
 	public static void main(String[] args) {
+		
+		//load model
+		PersistenceFoodTruck.loadFoodTruckModel();
 		
 		//initialize view
 		FoodTruckManagementPage.main(args);
