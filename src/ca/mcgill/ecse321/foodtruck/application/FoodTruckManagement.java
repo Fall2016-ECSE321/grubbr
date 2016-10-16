7eed34b7ff2a113325a8795db003af281501d3c0
erick <erick@hotmail.ca>
2016-10-15 22:19:26 -0400

    Add basic GUI for Add Menu Item use case
    There are now two TextFields to specify an item name and price,
    and a button to add it to the menu. The functionality in itself
    is still not coded.

---
+++ src/ca/mcgill/ecse321/foodtruck/application/FoodTruckManagement.java
@@ -0,0 +1,14 @@
+package ca.mcgill.ecse321.foodtruck.application;
+
+import ca.mcgill.ecse321.foodtruck.view.FoodTruckManagementPage;
+
+public class FoodTruckManagement {
+	
+	public static void main(String[] args) {
+		
+		//initialize view
+		FoodTruckManagementPage.main(args);
+		
+	}
+
+}
---
+++ src/ca/mcgill/ecse321/foodtruck/view/FoodTruckManagementPage.java
@@ -0,0 +1,110 @@
+package ca.mcgill.ecse321.foodtruck.view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import javax.swing.SwingConstants;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JButton;
+
+public class FoodTruckManagementPage {
+
+	private JFrame frame;
+	private JTextField txtItemName;
+	private JTextField txtItemPrice;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FoodTruckManagementPage window = new FoodTruckManagementPage();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public FoodTruckManagementPage() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 488, 237);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.WEST);
+		
+		JLabel lblItemName = new JLabel("Item Name");
+		
+		txtItemName = new JTextField();
+		txtItemName.setColumns(10);
+		
+		JLabel lblItemPrice = new JLabel("Item Price");
+		
+		txtItemPrice = new JTextField();
+		txtItemPrice.setColumns(10);
+		
+		JButton btnAddToMenu = new JButton("Add to menu");
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGap(5)
+							.addComponent(lblItemName)
+							.addPreferredGap(ComponentPlacement.RELATED, 7, Short.MAX_VALUE)
+							.addComponent(txtItemName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(lblItemPrice)
+							.addPreferredGap(ComponentPlacement.RELATED, 12, Short.MAX_VALUE)
+							.addComponent(txtItemPrice, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(btnAddToMenu))
+					.addContainerGap())
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGap(10)
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblItemName)
+						.addComponent(txtItemName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblItemPrice)
+						.addComponent(txtItemPrice, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addComponent(btnAddToMenu)
+					.addContainerGap(151, Short.MAX_VALUE))
+		);
+		panel.setLayout(gl_panel);
+	}
+	
+	/**
+	 * Refresh data to update the view
+	 */
+	
+	public void refreshData() {
+		
+	}
+}
