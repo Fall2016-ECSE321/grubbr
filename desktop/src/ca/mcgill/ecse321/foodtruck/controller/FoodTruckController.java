41c7ba494b25bb1d790f821852ff588c52572ad0
erick <erick@hotmail.ca>
2016-10-27 20:37:30 -0400

    Desktop: Add create/edit inventory methods
    There are now methods in the FoodTruckController to create and edit
    food supply and equipment counts in the inventory. Each item is
    instantiated with a quantity of 0, and it is up to the user to define
    the quantity manually. Deleting is part of the edit function as well;
    inputting a negative integer will delete the item from the inventory
    altogether.

--- desktop/src/ca/mcgill/ecse321/foodtruck/controller/FoodTruckController.java
+++ desktop/src/ca/mcgill/ecse321/foodtruck/controller/FoodTruckController.java
@@ -1,14 +1,18 @@
 package ca.mcgill.ecse321.foodtruck.controller;
 
+import org.apache.commons.lang3.text.WordUtils;
+
+import ca.mcgill.ecse321.foodtruck.model.Equipment;
 import ca.mcgill.ecse321.foodtruck.model.FoodTruckManager;
 import ca.mcgill.ecse321.foodtruck.model.MenuItem;
+import ca.mcgill.ecse321.foodtruck.model.Supply;
 import ca.mcgill.ecse321.foodtruck.persistence.PersistenceXStream;
 
 /**
  * Controller for the Food Truck Management System.
  * 
  * @author erick
- * @version 0.1
+ * @version 0.2
  *
  */
 
@@ -17,6 +21,7 @@ public class FoodTruckController {
 	/**
 	 * Constructor.
 	 */
+	
 	public FoodTruckController() {
 	}
 	
@@ -56,6 +61,78 @@ public class FoodTruckController {
 		FoodTruckManager ftms = FoodTruckManager.getInstance();
 		ftms.addMenuItem(item);
 		PersistenceXStream.saveToXMLwithXStream(ftms);
+	}
+	
+	/**
+	 * Adds a new supply to the supply list.
+	 * 
+	 * Each supply has a user-defined name and a count. 
+	 * By default, the count of a supply is 0, and it is up to the
+	 * user to instantiate its initial quantity.
+	 * @param supplyName	Name of the food supply in question.
+	 */
+	
+	public void createSupply(String supplyName) {
+		
+		supplyName = WordUtils.capitalizeFully(supplyName);
+		Supply supply = new Supply(supplyName,0);
+		FoodTruckManager ftms = FoodTruckManager.getInstance();
+		ftms.addSupply(supply);
+		PersistenceXStream.saveToXMLwithXStream(ftms);
+	}
+	
+	/**
+	 * Sets the quantity of an existing food supply in the inventory.
+	 * 
+	 * A value under 0 will give the command to delete the item entirely.
+	 * @param supply	Food supply whose quantity is to be edited.
+	 * @param count		Quantity to be set.
+	 */
+	
+	public void editSupplyQuantity(Supply supply,int count) {
+		
+		if (count < 0) {
+			supply.delete();
+		} else {
+			supply.setCount(count);
+		}
+		
+	}
+	
+	/**
+	 * Adds a new equipment to the equipment list.
+	 * 
+	 * Each piece of equipment has a user-defined name and a count. 
+	 * By default, the count of an equipment is 0, and it is up to the
+	 * user to instantiate its initial quantity.
+	 * @param equipmentName
+	 */
+	
+	public void createEquipment(String equipmentName) {
+		
+		equipmentName = WordUtils.capitalizeFully(equipmentName);
+		Equipment equipment = new Equipment(equipmentName,0);
+		FoodTruckManager ftms = FoodTruckManager.getInstance();
+		ftms.addEquipment(equipment);
+		PersistenceXStream.saveToXMLwithXStream(ftms);
+	}
+	
+	/**
+	 * Sets the quantity of an existing equipment in the inventory.
+	 * 
+	 * A value under 0 will give the command to delete the item entirely.
+	 * @param equipment	Equipment whose quantity is to be edited.
+	 * @param count		Quantity to be set.
+	 */
+	
+	public void editEquipmentQuantity(Equipment equipment,int count) {
+		
+		if (count < 0) {
+			equipment.delete();
+		} else {
+			equipment.setCount(count);
+		}
+		
 	}
 	
 	/**
